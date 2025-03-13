d743c9f4d04b52b731209f3afc1cb381adc3555c
Gerard Olenski <gerard.olenski@hyland.com>
2025-03-13 13:47:59 +0100

    Revert "pre commit format poc"
    This reverts commit 61a209271734831605e0592acacc1590fa54e21e.

--- scripts/hooks/check-format-and-headers.sh
+++ scripts/hooks/check-format-and-headers.sh
@@ -2,7 +2,21 @@
 
 set +x
 
-mvn spotless:apply validate -DlicenseUpdateHeaders=true -Pags,all-tas-tests > /dev/null || true
+if [[ -z ${GITHUB_MODIFIED_FILES} ]]
+then
+  modified_files=$(git diff --cached --name-only --diff-filter=ACMR)
+else
+  modified_files=${GITHUB_MODIFIED_FILES}
+fi
+
+include_list=""
+for file in ${modified_files}
+do
+  include_list="${include_list},${file}"
+done
+include_list=${include_list:1}
+
+mvn spotless:apply validate -DlicenseUpdateHeaders=true -Pags,all-tas-tests -Dspotless-include-list="${include_list}" > /dev/null || true
 
 all_nonconformant_files=$(git diff --name-only --diff-filter=ACMR)
 
