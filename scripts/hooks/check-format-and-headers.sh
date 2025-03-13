ecd1b8b2bb1dae8cdb336d798f9f0648ca60f075
Gerard Olenski <gerard.olenski@hyland.com>
2025-03-13 13:47:59 +0100

    Revert "pre commit format poc"
    This reverts commit f8c3a155ac8fa3db18703565a7c762cebf7c23c0.

--- scripts/hooks/check-format-and-headers.sh
+++ scripts/hooks/check-format-and-headers.sh
@@ -4,4 +4,23 @@ set +x
 
 mvn spotless:apply validate -DlicenseUpdateHeaders=true -Pags,all-tas-tests > /dev/null || true
 
+all_nonconformant_files=$(git diff --name-only --diff-filter=ACMR)
+
+for file in ${all_nonconformant_files}
+do
+  revert=1
+  for modified_file in ${modified_files}
+  do
+    if [[ "${modified_file}" == "${file}" ]]
+    then
+      revert=0
+      break
+    fi
+  done
+  if [[ ${revert} == 1 ]]
+  then
+    git checkout -- "${file}"
+  fi
+done
+
 set -x
